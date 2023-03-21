a4bdb55f0cbb6ae6a9d794f2a8a86c61e9080a30
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-03-21 17:03:09 +0100

    change gui&res cmake impl

---
+++ ext/raygui.c
@@ -0,0 +1,5 @@
+#define RAYGUI_IMPLEMENTATION
+#include "raygui/src/raygui.h"
+
+#define GUI_TEXTBOX_EXTENDED_IMPLEMENTATION
+#include "raygui/src/extras/gui_textbox_extended.h"
---
+++ ext/rres.c
@@ -0,0 +1,8 @@
+#include <raylib.h>
+
+#define RRES_IMPLEMENTATION
+#define RRES_RAYLIB_IMPLEMENTATION
+#define RRES_SUPPORT_COMPRESSION_LZ4
+#define RRES_SUPPORT_ENCRYPTION_AES
+#define RRES_SUPPORT_ENCRYPTION_XCHACHA20
+#include "rres/src/rres-raylib.h"
