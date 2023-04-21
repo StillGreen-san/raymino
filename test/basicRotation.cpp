ffe087740ccd1c8716fd03dc351aca8d5bf2de22
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-04-21 17:40:54 +0200

    change testcase setup for basicRotation

--- test/basicRotation.cpp
+++ test/basicRotation.cpp
@@ -7,49 +7,46 @@ using namespace raymino;
 TEST_CASE("basicRotation<Super>", "[basicRotation]")
 {
 	std::vector<Tetromino> tetrominos = makeBaseMinos<RotationSystem::Super>();
+	Tetromino& minoI = tetrominos[0];
+	Tetromino& minoJ = tetrominos[1];
+	Tetromino& minoS = tetrominos[4];
 
 	{
-		const Offset offset = basicRotation<RotationSystem::Super>(tetrominos[0], 2);
-		REQUIRE(offset == Offset{{0, 0}, 2});
-		tetrominos[0] += offset;
+		const Offset offset = basicRotation<RotationSystem::Super>(minoI, 2);
+		minoI += offset;
 		const Grid negative({4, 4}, {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1});
-		REQUIRE(negative.overlapAt(tetrominos[0].position, tetrominos[0].collision) == false);
+		REQUIRE(negative.overlapAt(minoI.position, minoI.collision) == false);
 	}
 	{
-		const Offset offset = basicRotation<RotationSystem::Super>(tetrominos[0], -1);
-		REQUIRE(offset == Offset{{0, 0}, -1});
-		tetrominos[0] += offset;
+		const Offset offset = basicRotation<RotationSystem::Super>(minoI, -1);
+		minoI += offset;
 		const Grid negative({4, 4}, {1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1});
-		REQUIRE(negative.overlapAt(tetrominos[0].position, tetrominos[0].collision) == false);
+		REQUIRE(negative.overlapAt(minoI.position, minoI.collision) == false);
 	}
 
 	{
-		const Offset offset = basicRotation<RotationSystem::Super>(tetrominos[1], 3);
-		REQUIRE(offset == Offset{{0, 0}, 3});
-		tetrominos[1] += offset;
+		const Offset offset = basicRotation<RotationSystem::Super>(minoJ, 3);
+		minoJ += offset;
 		const Grid negative({3, 3}, {1, 0, 1, 1, 0, 1, 0, 0, 1});
-		REQUIRE(negative.overlapAt(tetrominos[1].position, tetrominos[1].collision) == false);
+		REQUIRE(negative.overlapAt(minoJ.position, minoJ.collision) == false);
 	}
 	{
-		const Offset offset = basicRotation<RotationSystem::Super>(tetrominos[1], 1);
-		REQUIRE(offset == Offset{{0, 0}, 1});
-		tetrominos[1] += offset;
+		const Offset offset = basicRotation<RotationSystem::Super>(minoJ, 1);
+		minoJ += offset;
 		const Grid negative({3, 3}, {0, 1, 1, 0, 0, 0, 1, 1, 1});
-		REQUIRE(negative.overlapAt(tetrominos[1].position, tetrominos[1].collision) == false);
+		REQUIRE(negative.overlapAt(minoJ.position, minoJ.collision) == false);
 	}
 
 	{
-		const Offset offset = basicRotation<RotationSystem::Super>(tetrominos[4], -1);
-		REQUIRE(offset == Offset{{0, 0}, -1});
-		tetrominos[4] += offset;
+		const Offset offset = basicRotation<RotationSystem::Super>(minoS, -1);
+		minoS += offset;
 		const Grid negative({3, 3}, {0, 1, 1, 0, 0, 1, 1, 0, 1});
-		REQUIRE(negative.overlapAt(tetrominos[4].position, tetrominos[4].collision) == false);
+		REQUIRE(negative.overlapAt(minoS.position, minoS.collision) == false);
 	}
 	{
-		const Offset offset = basicRotation<RotationSystem::Super>(tetrominos[4], -3);
-		REQUIRE(offset == Offset{{0, 0}, -3});
-		tetrominos[4] += offset;
+		const Offset offset = basicRotation<RotationSystem::Super>(minoS, -3);
+		minoS += offset;
 		const Grid negative({3, 3}, {1, 0, 0, 0, 0, 1, 1, 1, 1});
-		REQUIRE(negative.overlapAt(tetrominos[4].position, tetrominos[4].collision) == false);
+		REQUIRE(negative.overlapAt(minoS.position, minoS.collision) == false);
 	}
 }
