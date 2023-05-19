2a20dc249e865c9dab9b722c880ecfcdc066e70c
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-05-19 12:42:01 +0200

    add menu gui

--- inc/menu.hpp
+++ inc/menu.hpp
@@ -2,11 +2,107 @@
 
 #include "scenes.hpp"
 
+#include <raylib.h>
+
 namespace raymino
 {
 struct Menu : public IScene
 {
 	Menu();
 	void UpdateDraw(class App& app) override;
+
+	const char* GroupBoxGameText = "Game";
+	const char* ButtonStartGameText = "Start Game";
+	const char* ButtonHighscoresText = "Highscores";
+	const char* GroupBoxSettingsText = "Settings";
+	const char* LabelRotationSystemText = "Rotation System";
+	const char* LabelWallKicksText = "Wall Kicks";
+	const char* LabelLockDownText = "Lock Down";
+	const char* LabelSoftDropText = "Soft Drop";
+	const char* LabelInstantDropText = "Instant Drop";
+	const char* LabelTSpinText = "TSpin";
+	const char* LabelShuffleTypeText = "Shuffle Type";
+	const char* LabelScoringSystemText = "Scoring System";
+	const char* LabelFieldWidthText = "Field Width";
+	const char* LabelFieldHeightText = "Field Height";
+	const char* LabelPreviewCountText = "Preview Count";
+	const char* LabelHoldPieceText = "Hold Piece";
+	const char* LabelPresetsText = "Presets";
+	const char* DropdownBoxRotationSystemText = "Nintendo Right;PlaceHolder";
+	const char* DropdownBoxWallKicksText = "Arika;PlaceHolder";
+	const char* DropdownBoxLockDownText = "Extended;PlaceHolder";
+	const char* DropdownBoxSoftDropText = "None Locking;PlaceHolder";
+	const char* DropdownBoxInstantDropText = "Sonic;PlaceHolder";
+	const char* DropdownBoxTSpinText = "Three Corner;PlaceHolder";
+	const char* DropdownBoxShuffleTypeText = "Single Bag;PlaceHolder";
+	const char* DropdownBoxScoringSystemText = "Guideline;PlaceHolder";
+	const char* DropdownBoxHoldPieceText = "Yes;PlaceHolder";
+	const char* DropdownBoxPresetsText = "Guideline;PlaceHolder";
+
+	const Vector2 AnchorGame = {24, 24};
+	const Vector2 AnchorSettings = {24, 120};
+
+	bool DropdownBoxRotationSystemEditMode = false;
+	bool DropdownBoxWallKicksEditMode = false;
+	bool DropdownBoxLockDownEditMode = false;
+	bool DropdownBoxSoftDropEditMode = false;
+	bool DropdownBoxInstantDropEditMode = false;
+	bool DropdownBoxTSpinEditMode = false;
+	bool DropdownBoxShuffleTypeEditMode = false;
+	bool DropdownBoxScoringSystemEditMode = false;
+	bool DropdownBoxHoldPieceEditMode = false;
+	bool SpinnerPreviewCountEditMode = false;
+	bool SpinnerFieldWidthEditMode = false;
+	bool SpinnerFieldHeightEditMode = false;
+	bool DropdownBoxPresetsEditMode = false;
+	bool ButtonGameStartClicked = false;
+	bool ButtonHighscoresClicked = false;
+
+	int DropdownBoxRotationSystemActive = 0;
+	int DropdownBoxWallKicksActive = 0;
+	int DropdownBoxLockDownActive = 0;
+	int DropdownBoxSoftDropActive = 0;
+	int DropdownBoxInstantDropActive = 0;
+	int DropdownBoxTSpinActive = 0;
+	int DropdownBoxShuffleTypeActive = 0;
+	int DropdownBoxScoringSystemActive = 0;
+	int DropdownBoxHoldPieceActive = 0;
+	int SpinnerPreviewCountValue = 0;
+	int SpinnerFieldWidthValue = 0;
+	int SpinnerFieldHeightValue = 0;
+	int DropdownBoxPresetsActive = 0;
+
+	const Rectangle layoutRecs[30] = {
+	    {AnchorGame.x + 0, AnchorGame.y + 0, 552, 72},
+	    {AnchorSettings.x + 0, AnchorSettings.y + 0, 552, 456},
+	    {AnchorGame.x + 88, AnchorGame.y + 16, 152, 40},
+	    {AnchorSettings.x + 24, AnchorSettings.y + 72, 96, 24},
+	    {AnchorSettings.x + 24, AnchorSettings.y + 120, 96, 24},
+	    {AnchorSettings.x + 24, AnchorSettings.y + 168, 96, 24},
+	    {AnchorSettings.x + 24, AnchorSettings.y + 216, 96, 24},
+	    {AnchorSettings.x + 24, AnchorSettings.y + 264, 96, 24},
+	    {AnchorSettings.x + 24, AnchorSettings.y + 312, 96, 24},
+	    {AnchorSettings.x + 24, AnchorSettings.y + 360, 96, 24},
+	    {AnchorSettings.x + 24, AnchorSettings.y + 408, 96, 24},
+	    {AnchorSettings.x + 120, AnchorSettings.y + 72, 136, 24},
+	    {AnchorSettings.x + 120, AnchorSettings.y + 120, 136, 24},
+	    {AnchorSettings.x + 120, AnchorSettings.y + 168, 136, 24},
+	    {AnchorSettings.x + 120, AnchorSettings.y + 216, 136, 24},
+	    {AnchorSettings.x + 120, AnchorSettings.y + 264, 136, 24},
+	    {AnchorSettings.x + 120, AnchorSettings.y + 312, 136, 24},
+	    {AnchorSettings.x + 120, AnchorSettings.y + 360, 136, 24},
+	    {AnchorSettings.x + 120, AnchorSettings.y + 408, 136, 24},
+	    {AnchorSettings.x + 296, AnchorSettings.y + 72, 96, 24},
+	    {AnchorSettings.x + 296, AnchorSettings.y + 120, 96, 24},
+	    {AnchorSettings.x + 296, AnchorSettings.y + 168, 96, 24},
+	    {AnchorSettings.x + 296, AnchorSettings.y + 216, 96, 24},
+	    {AnchorSettings.x + 392, AnchorSettings.y + 216, 136, 24},
+	    {AnchorSettings.x + 392, AnchorSettings.y + 168, 136, 24},
+	    {AnchorSettings.x + 392, AnchorSettings.y + 72, 136, 24},
+	    {AnchorSettings.x + 392, AnchorSettings.y + 120, 136, 24},
+	    {AnchorGame.x + 312, AnchorGame.y + 16, 152, 40},
+	    {AnchorSettings.x + 120, AnchorSettings.y + 16, 272, 32},
+	    {AnchorSettings.x + 24, AnchorSettings.y + 16, 96, 24},
+	};
 };
 } // namespace raymino
--- src/menu.cpp
+++ src/menu.cpp
@@ -15,20 +15,112 @@ std::unique_ptr<IScene> MakeScene<Scene::Menu>([[maybe_unused]] App& app)
 
 Menu::Menu()
 {
-	::GuiSetStyle(::GuiControl::DEFAULT, ::GuiDefaultProperty::TEXT_SIZE, 30);
 }
 
 void Menu::UpdateDraw(App& app)
 {
-	::BeginDrawing();
+	BeginDrawing();
 
-	::ClearBackground(LIGHTGRAY);
+	ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-	if(::GuiButton(Rectangle{60, 60, 210, 35}, "START GAME"))
+	if(DropdownBoxRotationSystemEditMode || DropdownBoxWallKicksEditMode || DropdownBoxLockDownEditMode ||
+	    DropdownBoxSoftDropEditMode || DropdownBoxInstantDropEditMode || DropdownBoxTSpinEditMode ||
+	    DropdownBoxShuffleTypeEditMode || DropdownBoxScoringSystemEditMode || DropdownBoxHoldPieceEditMode ||
+	    DropdownBoxPresetsEditMode)
 	{
-		app.SwitchScene(MakeScene<Scene::Game>(app));
+		GuiLock();
 	}
 
-	::EndDrawing();
+	GuiGroupBox(layoutRecs[0], GroupBoxGameText);
+	ButtonGameStartClicked = GuiButton(layoutRecs[2], ButtonStartGameText);
+	ButtonHighscoresClicked = GuiButton(layoutRecs[27], ButtonHighscoresText);
+
+	GuiGroupBox(layoutRecs[1], GroupBoxSettingsText);
+	GuiLabel(layoutRecs[3], LabelRotationSystemText);
+	GuiLabel(layoutRecs[4], LabelWallKicksText);
+	GuiLabel(layoutRecs[5], LabelLockDownText);
+	GuiLabel(layoutRecs[6], LabelSoftDropText);
+	GuiLabel(layoutRecs[7], LabelInstantDropText);
+	GuiLabel(layoutRecs[8], LabelTSpinText);
+	GuiLabel(layoutRecs[9], LabelShuffleTypeText);
+	GuiLabel(layoutRecs[10], LabelScoringSystemText);
+	GuiLabel(layoutRecs[19], LabelFieldWidthText);
+	GuiLabel(layoutRecs[20], LabelFieldHeightText);
+	GuiLabel(layoutRecs[21], LabelPreviewCountText);
+	GuiLabel(layoutRecs[22], LabelHoldPieceText);
+	GuiLabel(layoutRecs[29], LabelPresetsText);
+	if(GuiSpinner(layoutRecs[24], "", &SpinnerPreviewCountValue, 0, 10, SpinnerPreviewCountEditMode))
+	{
+		SpinnerPreviewCountEditMode = !SpinnerPreviewCountEditMode;
+	}
+	if(GuiSpinner(layoutRecs[25], "", &SpinnerFieldWidthValue, 5, 100, SpinnerFieldWidthEditMode))
+	{
+		SpinnerFieldWidthEditMode = !SpinnerFieldWidthEditMode;
+	}
+	if(GuiSpinner(layoutRecs[26], "", &SpinnerFieldHeightValue, 10, 100, SpinnerFieldHeightEditMode))
+	{
+		SpinnerFieldHeightEditMode = !SpinnerFieldHeightEditMode;
+	}
+	if(GuiDropdownBox(
+	       layoutRecs[23], DropdownBoxHoldPieceText, &DropdownBoxHoldPieceActive, DropdownBoxHoldPieceEditMode))
+	{
+		DropdownBoxHoldPieceEditMode = !DropdownBoxHoldPieceEditMode;
+	}
+	if(GuiDropdownBox(layoutRecs[18], DropdownBoxScoringSystemText, &DropdownBoxScoringSystemActive,
+	       DropdownBoxScoringSystemEditMode))
+	{
+		DropdownBoxScoringSystemEditMode = !DropdownBoxScoringSystemEditMode;
+	}
+	if(GuiDropdownBox(
+	       layoutRecs[17], DropdownBoxShuffleTypeText, &DropdownBoxShuffleTypeActive, DropdownBoxShuffleTypeEditMode))
+	{
+		DropdownBoxShuffleTypeEditMode = !DropdownBoxShuffleTypeEditMode;
+	}
+	if(GuiDropdownBox(layoutRecs[16], DropdownBoxTSpinText, &DropdownBoxTSpinActive, DropdownBoxTSpinEditMode))
+	{
+		DropdownBoxTSpinEditMode = !DropdownBoxTSpinEditMode;
+	}
+	if(GuiDropdownBox(
+	       layoutRecs[15], DropdownBoxInstantDropText, &DropdownBoxInstantDropActive, DropdownBoxInstantDropEditMode))
+	{
+		DropdownBoxInstantDropEditMode = !DropdownBoxInstantDropEditMode;
+	}
+	if(GuiDropdownBox(layoutRecs[14], DropdownBoxSoftDropText, &DropdownBoxSoftDropActive, DropdownBoxSoftDropEditMode))
+	{
+		DropdownBoxSoftDropEditMode = !DropdownBoxSoftDropEditMode;
+	}
+	if(GuiDropdownBox(layoutRecs[13], DropdownBoxLockDownText, &DropdownBoxLockDownActive, DropdownBoxLockDownEditMode))
+	{
+		DropdownBoxLockDownEditMode = !DropdownBoxLockDownEditMode;
+	}
+	if(GuiDropdownBox(
+	       layoutRecs[12], DropdownBoxWallKicksText, &DropdownBoxWallKicksActive, DropdownBoxWallKicksEditMode))
+	{
+		DropdownBoxWallKicksEditMode = !DropdownBoxWallKicksEditMode;
+	}
+	if(GuiDropdownBox(layoutRecs[11], DropdownBoxRotationSystemText, &DropdownBoxRotationSystemActive,
+	       DropdownBoxRotationSystemEditMode))
+	{
+		DropdownBoxRotationSystemEditMode = !DropdownBoxRotationSystemEditMode;
+	}
+	if(GuiDropdownBox(layoutRecs[28], DropdownBoxPresetsText, &DropdownBoxPresetsActive, DropdownBoxPresetsEditMode))
+	{
+		DropdownBoxPresetsEditMode = !DropdownBoxPresetsEditMode;
+	}
+
+	GuiUnlock();
+
+	EndDrawing();
+
+	if(ButtonGameStartClicked)
+	{
+		app.SwitchScene(MakeScene<Scene::Game>(app));
+		return;
+	}
+	if(ButtonHighscoresClicked)
+	{
+		//!		app.SwitchScene(MakeScene<Scene::Highscores>(app));
+		return;
+	}
 }
 } // namespace raymino
