5523dc3fcb2f89370011f7d8d2427424a11affda
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-05-19 16:48:27 +0200

    add Settings object to App

--- inc/app.hpp
+++ inc/app.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "scenes.hpp"
+#include "types.hpp"
 
 #include <memory>
 
@@ -12,6 +13,22 @@ namespace raymino
 class App
 {
 public:
+	struct Settings
+	{
+		RotationSystem rotationSystem = RotationSystem::Super;
+		WallKicks wallKicks = WallKicks::Super;
+		LockDown lockDown = LockDown::Extended;
+		SoftDrop softDrop = SoftDrop::NonLocking;
+		InstantDrop instantDrop = InstantDrop::Hard;
+		TSpin tSpin = TSpin::ThreeCorner;
+		ShuffleType shuffleType = ShuffleType::SingleBag;
+		ScoringSystem scoringSystem = ScoringSystem::Guideline;
+		bool holdPiece = true;
+		int fieldWidth = 10;
+		int fieldHeight = 20;
+		int previewCount = 6;
+	};
+
 	/**
 	 * @brief create an instance (only one should exist)
 	 * @tparam TScene starting scene enum
@@ -39,6 +56,8 @@ public:
 	 * @param newScene to switch to
 	 */
 	void QueueSceneSwitch(std::unique_ptr<IScene> newScene);
+
+	Settings settings;
 
 private:
 	App();
