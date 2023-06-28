b3ea2aa04cf9edf81eca6afdeb33ed0ae8a0412f
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-06-28 12:38:03 +0200

    add highscore loading

--- src/loading.cpp
+++ src/loading.cpp
@@ -5,6 +5,10 @@
 #include <raygui.h>
 #include <raylib-cpp.hpp>
 
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
+
 namespace raymino
 {
 template<>
@@ -15,6 +19,32 @@ std::unique_ptr<IScene> MakeScene<Scene::Loading>(App& app)
 
 Loading::Loading(App& app)
 {
+#if defined(PLATFORM_WEB)
+	emscripten_idb_async_load(
+	    App::IDB_PATH, App::FILE_PATH, &app,
+	    [](void* user, void* data, int size)
+	    {
+		    App& app = *static_cast<App*>(user);
+		    app.deserialize(static_cast<uint8_t*>(data), size);
+		    app.QueueSceneSwitch(MakeScene<Scene::Menu>(app));
+		    ::TraceLog(LOG_INFO, "FILEIO: [%s] File loaded successfully", App::FILE_PATH);
+	    },
+	    [](void* user)
+	    {
+		    App& app = *static_cast<App*>(user);
+		    app.QueueSceneSwitch(MakeScene<Scene::Menu>(app));
+		    ::TraceLog(LOG_INFO, "FILEIO: [%s] Failed to load file", App::FILE_PATH);
+	    });
+#else
+	unsigned bytes{};
+	unsigned char* fileData = ::LoadFileData(App::FILE_PATH, &bytes);
+	if(fileData)
+	{
+		app.deserialize(fileData, bytes);
+		::UnloadFileData(fileData);
+	}
+	app.QueueSceneSwitch(MakeScene<Scene::Menu>(app));
+#endif
 }
 
 void Loading::UpdateDraw(App& app)
