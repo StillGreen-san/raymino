c57fd57e5a99b4833408840a6614445dfcc4ce30
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-07-03 14:52:54 +0200

    add about console output at startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include "app.hpp"
+#include "dependency_info.hpp"
+
+#include <iomanip>
+#include <iostream>
 
 #ifdef WIN32
 int WinMain()
@@ -6,6 +10,15 @@ int WinMain()
 int main()
 #endif
 {
+	std::cout << PROJECT_INFO.url << " [" << PROJECT_INFO.lic << ']' << std::endl;
+	std::cout << "  build with:" << std::endl;
+	for(const LibraryInfo& depInfo : DEPENDENCY_INFOS)
+	{
+		std::cout << depInfo.url;
+		std::cout << std::setw(static_cast<std::streamsize>(DEPENDENCY_INFOS_MAX_URL_LEN - depInfo.url.size()) + 2);
+		std::cout << '[' << depInfo.lic << ']' << std::endl;
+	}
+
 	raymino::App app;
 
 	app.Run();
