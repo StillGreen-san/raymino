8476426e338d57efa2f6c626e9a0ae3f158a8320
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-07-03 14:47:50 +0200

    add dependency_info.hpp

---
+++ inc/dependency_info.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <string_view>
+
+struct LibraryInfo
+{
+	std::string_view url;
+	std::string_view lic;
+};
+constexpr LibraryInfo PROJECT_INFO{"https://github.com/StillGreen-san/raymino", "MIT"};
+constexpr std::array<LibraryInfo, 5> DEPENDENCY_INFOS{{
+    {"https://github.com/raysan5/raylib", "Zlib"},
+    {"https://github.com/RobLoach/raylib-cpp", "Zlib"},
+    {"https://github.com/raysan5/raygui", "Zlib"},
+    {"https://github.com/raysan5/rres", "MIT"},
+    {"https://github.com/Neargye/magic_enum", "MIT"},
+}};
+constexpr size_t DEPENDENCY_INFOS_MAX_URL_LEN = std::max_element(DEPENDENCY_INFOS.begin(), DEPENDENCY_INFOS.end(),
+    [](const auto& lhs, const auto& rhs)
+    {
+	    return lhs.url.size() < rhs.url.size();
+    }) -> url.size();
