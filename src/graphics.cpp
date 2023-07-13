f5430218eb1cf6256f9b3776f3255cbba84883ec
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-07-13 09:26:10 +0200

    fix horizontal line drawing height on some platforms

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -76,7 +76,9 @@ void drawBackground(const Grid& grid, XY at, int cellSize, int borderSize, Color
 	for(int hLine = 1; hLine < gridSize.height; ++hLine)
 	{
 		const int yOffset = (hLine * cellSize) + ((hLine - 1) * borderSize);
-		::DrawLine(at.x, at.y + yOffset, at.x + totalWidth, at.y + yOffset, lines);
+		const float magicYOffset = 0.5f; // + 0.5f offset needed on some platforms for some reason
+		::DrawLineEx({static_cast<float>(at.x), static_cast<float>(at.y + yOffset) + magicYOffset},
+		    {static_cast<float>(at.x + totalWidth), static_cast<float>(at.y + yOffset) + magicYOffset}, 1.f, lines);
 	}
 }
 } // namespace raymino
