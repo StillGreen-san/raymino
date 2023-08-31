fbd9858219f1fdabe5818634501d11c96589d3f3
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-08-31 12:38:50 +0200

    TextList impl

--- src/gui.cpp
+++ src/gui.cpp
@@ -2,22 +2,65 @@
 
 namespace raymino
 {
+size_t find_nth(const std::string& str, size_t nth, char chr)
+{
+	size_t pos = nth == 0 ? 0 : -1;
+	for(; nth > 0; --nth)
+	{
+		pos = str.find(chr, pos + 1);
+		if(pos == std::string::npos)
+		{
+			break;
+		}
+	}
+	return pos;
+}
+
 void TextList::add(std::string_view items)
 {
+	if(!items.empty())
+	{
+		if(!list.empty())
+		{
+			list.append(1, delimiter);
+		}
+		list.append(items);
+	}
 }
 void TextList::remove(size_t index)
 {
+	const size_t delimPos = find_nth(list, index, delimiter);
+	if(delimPos == std::string::npos)
+	{
+		return;
+	}
+	const size_t itemPos = delimPos + (index == 0 ? 0 : 1);
+	const size_t itemEnd = list.find(delimiter, itemPos);
+	const size_t itemLen = (itemEnd == std::string::npos ? list.size() - itemPos : itemEnd - itemPos) + 1;
+	list.erase(list[delimPos] == delimiter ? delimPos : itemPos, itemLen);
 }
 std::string_view TextList::get(size_t index) const
 {
-	return {};
+	const size_t delimPos = find_nth(list, index, delimiter);
+	if(delimPos == std::string::npos)
+	{
+		return {};
+	}
+	const size_t itemPos = delimPos + (index == 0 ? 0 : 1);
+	const size_t itemEnd = list.find(delimiter, itemPos);
+	const size_t itemLen = itemEnd == std::string::npos ? list.size() - itemPos : itemEnd - itemPos;
+	return {&list[itemPos], itemLen};
 }
 std::string_view TextList::get() const
 {
-	return {};
+	return list;
 }
 size_t TextList::size() const
 {
-	return 0;
+	if(list.empty())
+	{
+		return 0;
+	}
+	return std::count(list.begin(), list.end(), delimiter) + 1;
 }
 } // namespace raymino
