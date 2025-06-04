084414f532a4025f49868bcbb1d3e934f21d9c76
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2025-06-04 13:23:37 +0200

    add CPM & catch2 to DEPENDENCY_INFOS

--- inc/dependency_info.hpp
+++ inc/dependency_info.hpp
@@ -14,12 +14,14 @@ struct LibraryInfo
 	CStringView lic;
 };
 constexpr LibraryInfo PROJECT_INFO{"https://github.com/StillGreen-san/raymino"_csv, "MIT"_csv};
-constexpr std::array<LibraryInfo, 5> DEPENDENCY_INFOS{{
+constexpr std::array<LibraryInfo, 7> DEPENDENCY_INFOS{{
     {"https://github.com/raysan5/raylib"_csv, "Zlib"_csv},
     {"https://github.com/RobLoach/raylib-cpp"_csv, "Zlib"_csv},
     {"https://github.com/raysan5/raygui"_csv, "Zlib"_csv},
     {"https://github.com/raysan5/rres"_csv, "MIT"_csv},
     {"https://github.com/Neargye/magic_enum"_csv, "MIT"_csv},
+    {"https://github.com/catchorg/Catch2"_csv, "BSL"_csv},
+    {"https://github.com/cpm-cmake"_csv, "MIT"_csv},
 }};
 constexpr size_t DEPENDENCY_INFOS_MAX_URL_LEN = std::max_element(DEPENDENCY_INFOS.begin(), DEPENDENCY_INFOS.end(),
     [](const auto& lhs, const auto& rhs)
