b2254446b2dc6a72a387c0128ed2f202041c70c5
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2025-06-06 16:11:47 +0200

    reformat cmake files

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -1,15 +1,15 @@
 # based on https://github.com/lefticus/cpp_starter_project/
 
-set( MSVC_WARNINGS
+set(MSVC_WARNINGS
 		/W4 # Baseline reasonable warnings
 		/w14242 # 'identifier': conversion from 'type1' to 'type1', possible loss of data
 		/w14254 # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data
 		/w14263 # 'function': member function does not override any base class virtual member function
 		/w14265 # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not
-						# be destructed correctly
+		# be destructed correctly
 		/w14287 # 'operator': unsigned/negative constant mismatch
 		/we4289 # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside
-						# the for-loop scope
+		# the for-loop scope
 		/w14296 # 'operator': expression is always 'boolean_value'
 		/w14311 # 'variable': pointer truncation from 'type1' to 'type2'
 		/w14545 # expression before comma evaluates to a function which is missing an argument list
@@ -26,32 +26,32 @@ set( MSVC_WARNINGS
 		/permissive- # standards conformance mode for MSVC compiler.
 )
 
-set( CLANG_WARNINGS
+set(CLANG_WARNINGS
 		-Wall
-        -Wextra # reasonable and standard
-        -Wextra-semi # Warn about semicolon after in-class function definition.
-        -Wshadow # warn the user if a variable declaration shadows one from a parent context
-        -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
-        					# catch hard to track down memory errors
-        -Wold-style-cast # warn for c-style casts
-        -Wcast-align # warn for potential performance problem casts
-        -Wunused # warn on anything being unused
-        -Woverloaded-virtual # warn if you overload (not override) a virtual function
-        -Wpedantic # warn if non-standard C++ is used
-        -Wconversion # warn on type conversions that may lose data
-        -Wsign-conversion # warn on sign conversions
-        -Wnull-dereference # warn if a null dereference is detected
-        -Wdouble-promotion # warn if float is implicit promoted to double
-        -Wformat=2 # warn on security issues around functions that format output (ie printf)
-        -Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
+		-Wextra # reasonable and standard
+		-Wextra-semi # Warn about semicolon after in-class function definition.
+		-Wshadow # warn the user if a variable declaration shadows one from a parent context
+		-Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
+		# catch hard to track down memory errors
+		-Wold-style-cast # warn for c-style casts
+		-Wcast-align # warn for potential performance problem casts
+		-Wunused # warn on anything being unused
+		-Woverloaded-virtual # warn if you overload (not override) a virtual function
+		-Wpedantic # warn if non-standard C++ is used
+		-Wconversion # warn on type conversions that may lose data
+		-Wsign-conversion # warn on sign conversions
+		-Wnull-dereference # warn if a null dereference is detected
+		-Wdouble-promotion # warn if float is implicit promoted to double
+		-Wformat=2 # warn on security issues around functions that format output (ie printf)
+		-Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
 )
 
-if( WARNINGS_AS_ERRORS )
-	set( CLANG_WARNINGS ${CLANG_WARNINGS} -Werror )
-	set( MSVC_WARNINGS ${MSVC_WARNINGS} /WX )
-endif( )
+if (WARNINGS_AS_ERRORS)
+	set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
+	set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
+endif ()
 
-set( GCC_WARNINGS
+set(GCC_WARNINGS
 		${CLANG_WARNINGS}
 		-Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
 		-Wduplicated-cond # warn if if / else chain has duplicated conditions
@@ -60,16 +60,16 @@ set( GCC_WARNINGS
 		-Wuseless-cast # warn if you perform a cast to the same type
 )
 
-if( MSVC )
-	add_compile_options( ${MSVC_WARNINGS} )
-elseif( CMAKE_CXX_COMPILER_ID MATCHES ".*Clang" )
-	add_compile_options( ${CLANG_WARNINGS} )
-elseif( CMAKE_CXX_COMPILER_ID STREQUAL "GNU" )
-	add_compile_options( ${GCC_WARNINGS} )
-else( )
-	message( AUTHOR_WARNING "No compiler warnings set for '${CMAKE_CXX_COMPILER_ID}' compiler." )
-endif( )
+if (MSVC)
+	add_compile_options(${MSVC_WARNINGS})
+elseif (CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+	add_compile_options(${CLANG_WARNINGS})
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+	add_compile_options(${GCC_WARNINGS})
+else ()
+	message(AUTHOR_WARNING "No compiler warnings set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
+endif ()
 
-unset( MSVC_WARNINGS )
-unset( CLANG_WARNINGS )
-unset( GCC_WARNINGS )
+unset(MSVC_WARNINGS)
+unset(CLANG_WARNINGS)
+unset(GCC_WARNINGS)
